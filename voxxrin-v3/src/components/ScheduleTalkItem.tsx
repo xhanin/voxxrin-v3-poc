757486126bb95b5afc056488ac74ea7063cc50f8
Xavier Hanin <xavier.hanin@4sh.fr>
2023-04-29 22:46:14 +0200

    add support for user talk notes

--- voxxrin-v3/src/components/ScheduleTalkItem.tsx
+++ voxxrin-v3/src/components/ScheduleTalkItem.tsx
@@ -1,9 +1,12 @@
 import {    
-    IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonBadge
+    IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonBadge, IonButton, IonItem, IonIcon
 } from '@ionic/react';
+import { star } from 'ionicons/icons';
 
 import {ScheduleTalk} from "../data/schedule"
+import useUserId from '../hooks/useUserId';
 import useTalkStats from '../hooks/useTalkStats';
+import useUserTalkNotes from '../hooks/useUserTalkNotes';
 
 interface ScheduleTalkItemProps {
     eventId: string,
@@ -12,18 +15,32 @@ interface ScheduleTalkItemProps {
 
 const ScheduleTalkItem: React.FC<ScheduleTalkItemProps> = ({eventId, talk}) => {
     const talkStats = useTalkStats({eventId: eventId, talkId: talk.id})
+    const userId = useUserId()
+    const {talkNotes, updateTalkNotes} = useUserTalkNotes({userId: userId, eventId: eventId, talkId: talk.id})
+    const isFavorite = talkNotes?.isFavorite ?? false
+
+    const toggleFavorite = function() {                
+        updateTalkNotes((notes) => {
+            notes.isFavorite = !notes.isFavorite
+        })
+    }
 
     return (
         <IonCard key={talk.id}>
         <IonCardHeader>
-          <IonCardTitle>{talk.title}</IonCardTitle>
+          <IonCardTitle>{talk.title}</IonCardTitle>          
           <IonCardSubtitle>{talk.track.title}</IonCardSubtitle>
-        </IonCardHeader>
-    
+        </IonCardHeader>        
+
         <IonCardContent>
-          {talk.room.title}
-          <IonBadge slot="end">{talkStats?.totalFavoritesCount ?? "0"}</IonBadge>
-        </IonCardContent>
+            <IonItem>
+                {talk.room.title}
+                <IonBadge slot="end">{talkStats?.totalFavoritesCount ?? "0"}</IonBadge>          
+                <IonButton slot="end" color={isFavorite ? "danger" : "light"} onClick={toggleFavorite}>
+                <IonIcon slot="icon-only" icon={star}></IonIcon>
+                </IonButton>
+            </IonItem>          
+        </IonCardContent>        
       </IonCard>
     );
 }
